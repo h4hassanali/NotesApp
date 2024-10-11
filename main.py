bbb3f61ed1107639d644ba48f88fe45cece25f88
h4hassanali <hxn112233@gmail.com>
2024-10-11 18:43:42 +0500

    Formatting done using black

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 from models import models
-from database.db import SessionLocal, engine 
+from database.db import SessionLocal, engine
 
 models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
+
 
 def get_db():
     db = SessionLocal()
@@ -12,6 +13,7 @@ def get_db():
         yield db
     finally:
         db.close()
+
 
 @app.post("/signup")
 def signup(name: str, email: str, password: str, db: Session = Depends(get_db)):
@@ -24,12 +26,14 @@ def signup(name: str, email: str, password: str, db: Session = Depends(get_db)):
     db.refresh(new_user)
     return new_user
 
+
 @app.get("/signin")
 def siginin(email: str, password: str, db: Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.email == email).first()
     if user:
         return f"Welcome {user.name}, you have Loged into the System"
     raise HTTPException(status_code=400, detail="User not found")
+
 
 @app.post("/add_note")
 def add_note(user_id: int, title: str, content: str, db: Session = Depends(get_db)):
@@ -41,6 +45,7 @@ def add_note(user_id: int, title: str, content: str, db: Session = Depends(get_d
     db.commit()
     db.refresh(new_note)
     return new_note
+
 
 @app.get("/read_note")
 def read_notes(user_id: int, db: Session = Depends(get_db)):
--- models/models.py
+++ models/models.py
@@ -1,6 +1,7 @@
-from sqlalchemy import Column, Integer,Text, ForeignKey, String, TIMESTAMP, text
+from sqlalchemy import Column, Integer, Text, ForeignKey, String, TIMESTAMP, text
 from database.db import Base
 from sqlalchemy.orm import relationship
+
 
 class User(Base):
     __tablename__ = "users"
@@ -9,7 +10,9 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, nullable=False)
     password = Column(String, nullable=False)
-    created_at = Column(TIMESTAMP(timezone=True), server_default=text('now()'), nullable=False)
+    created_at = Column(
+        TIMESTAMP(timezone=True), server_default=text("now()"), nullable=False
+    )
     notes = relationship("Note", back_populates="owner")
 
 
@@ -19,6 +22,8 @@ class Note(Base):
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, nullable=False)
     content = Column(Text, nullable=False)
-    created_at = Column(TIMESTAMP(timezone=True), server_default=text('now()'), nullable=False)
-    owner_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    created_at = Column(
+        TIMESTAMP(timezone=True), server_default=text("now()"), nullable=False
+    )
+    owner_id = Column(Integer, ForeignKey("users.id"), nullable=False)
     owner = relationship("User", back_populates="notes")
