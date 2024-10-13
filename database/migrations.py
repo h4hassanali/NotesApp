089218690de05cbda7bd9c3ba4d2b54698b6aa71
h4hassanali <hxn112233@gmail.com>
2024-10-13 13:22:10 +0500

    Exception handeling in migrate function.

--- database/migrations.py
+++ database/migrations.py
@@ -2,10 +2,16 @@
 from database.engine import engine
 from user.models import User
 from note.models import Note
+from sqlalchemy.exc import SQLAlchemyError
 
 
 # Create tables for User and Note models
 def migrate():
-    User.__table__.create(bind=engine, checkfirst=True)
-    Note.__table__.create(bind=engine, checkfirst=True)
-    print("Database tables created successfully.")
+    try:
+        User.__table__.create(bind=engine, checkfirst=True)
+        Note.__table__.create(bind=engine, checkfirst=True)
+        print("Database tables created successfully.")
+    except SQLAlchemyError as e:
+        print(f"An error occurred while creating tables: {e}")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
