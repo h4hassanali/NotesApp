45a0de7eb25f436c6ab000d81af111a61069b665
h4hassanali <hxn112233@gmail.com>
2024-10-29 16:30:24 +0500

    Refactor : config file

--- utils/configuration.py
+++ utils/configuration.py
@@ -1,17 +1,7 @@
-import logging
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
-
-LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO").upper()
-
-logging.basicConfig(
-    filename='user/user.log',
-    level=getattr(logging, LOG_LEVEL, logging.INFO),
-    format='%(asctime)s - %(levelname)s - %(message)s',
-)
-
 
 class Config:
     @staticmethod
